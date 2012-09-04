c352070b9f414c847251be69e6677c55f6164b2c
Brooks Swinnerton <bswinnerton@gmail.com>
2012-09-03 20:11:07 -0400

    bug fix for view

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -8,7 +8,7 @@ $environment = $this->config->item('storage') === 's3' ? 'http://'.$this->config
 <div id="containerSingleOuter">
 	<div id="containerSingleInner">
 		<?php foreach ($post as $postData): ?>
-	    <div class="contentBox"><?php echo formatType($postData['id_post'], $postData['type'], $postData['content'], $postData['width'], $postData['height'], 'single', $environment); ?></div>
+	    <div class="contentBox" id="noText"><?php echo formatType($postData['id_post'], $postData['type'], $postData['content'], $postData['width'], $postData['height'], 'single', $environment); ?></div>
 		<?php endforeach; ?>
 	</div>
 	<div class="clear"></div>
