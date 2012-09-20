e83172f615bc20fd2f94741ee9347cb14b9b2e49
Brooks Swinnerton <bswinnerton@gmail.com>
2012-09-19 20:09:45 -0400

    fix for #42

--- application/views/posts/page.php
+++ application/views/posts/page.php
@@ -3,8 +3,16 @@
 $this->load->helper('format');
 $environment = $this->config->item('storage') === 's3' ? 'http://'.$this->config->item('bucket', 's3') : '';
 		
-?>
 
-<?php foreach ($post as $postData): ?>
+foreach ($post as $postData): 
+	if ($postData['type'] == "text") {
+?>
+	<div class="contentBox"><?php echo formatType($postData['id_post'], $postData['type'], $postData['content'], $postData['width_thumbnail'], $postData['height_thumbnail'], 'all', $environment); ?></div>
+<?php 
+	} else {
+?>
 	<div class="contentBox" id="noText"><?php echo formatType($postData['id_post'], $postData['type'], $postData['content'], $postData['width_thumbnail'], $postData['height_thumbnail'], 'all', $environment); ?></div>
-<?php endforeach; ?>
+<?php 
+	}
+endforeach;
+?>
