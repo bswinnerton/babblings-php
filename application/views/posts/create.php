b5f900d80673744d2b6f4698b0439ab6de45d8cb
Brooks Swinnerton <bswinnerton@gmail.com>
2012-11-11 18:47:14 -0500

    text change for data submission

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -2,6 +2,6 @@
 	
 <div id="textbar">
 	<?php echo form_open('posts/create') ?>
-		<input type="text" name="content" value="enter your text, image, or video" onfocus="if (this.value == 'enter your text, image, or video') {this.value = '';}" onblur="if (this.value == '') {this.value = 'enter your text, image, or video';}" />
+		<input type="text" name="content" value="enter your website, image, or video" onfocus="if (this.value == 'enter your website, image, or video') {this.value = '';}" onblur="if (this.value == '') {this.value = 'enter your website, image, or video';}" />
 	</form>
 </div>
